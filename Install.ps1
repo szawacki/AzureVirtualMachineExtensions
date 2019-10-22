2cee37154b88b378c9a236414d187870d348a444
Stefan Zawacki <Stefan.Zawacki@outlook.com>
2019-10-22 14:32:52 +0200

    Create Install.ps1

---
+++ Install.ps1
@@ -0,0 +1,15 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $DomainName,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [Security.SecureString]
+    $Password
+)
+
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/szawacki/AzureVirtualMachineExtensions/master/InstallDomainController.ps1" -Description "Install.ps1"
+
+& Install.ps1 -DomainName $DomainName -Password Password
