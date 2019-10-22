a95c9852b6497eecb66f3f0943c8fcc441ab58c4
Stefan Zawacki <Stefan.Zawacki@outlook.com>
2019-10-22 14:37:43 +0200

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -10,6 +10,6 @@ Param(
     $Password
 )
 
-Start-BitsTransfer -Source "https://raw.githubusercontent.com/szawacki/AzureVirtualMachineExtensions/master/InstallDomainController.ps1" -Description "Install.ps1"
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/szawacki/AzureVirtualMachineExtensions/master/InstallDomainController.ps1" -Destination "C:\Install.ps1"
 
 #& Install.ps1 -DomainName $DomainName -Password Password
